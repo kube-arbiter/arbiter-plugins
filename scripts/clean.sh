24dd7648fa8e969058949c35a5730afce0bea850
0xff-dev <stevenshuang521@gmail.com>
2022-10-15 14:36:32 +0800

    fix: updating project dependencies

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/..
-rm -rf $ROOT/observer-plugins/metric-server/arbiter-metric-server;
-rm -rf $ROOT/observer-plugins/prometheus/arbiter-prometheus-server;
-rm -rf $ROOT/resource-tagger-plugin/resource-tagger;
-rm -rf $ROOT/resource-tagger-plugin/resource-tagger-client;
-docker rmi arbiter/arbiter-metric-server:$1 arbiter/arbiter-prometheus-server:$1 arbiter/resource-tagger-plugin:$1 2> /dev/null || true;
+rm -rf $ROOT/observer-plugins/metric-server/observer-metric-server;
+rm -rf $ROOT/observer-plugins/prometheus/observer-prometheus-server;
+rm -rf $ROOT/executor-plugins/resource-tagger/resource-tagger;
+rm -rf $ROOT/executor-plugins/resource-tagger/resource-tagger-client;
+docker rmi kubearbiter/observer-metric-server:$1 kubearbiter/observer-prometheus-server:$1 kubearbiter/kubearbiter/executor-resource-tagger:$1 2> /dev/null || true;
 

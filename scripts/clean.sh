c6e6ff7573dffc369423cc7e651bafd44ec73492
WangLei <nkwanglei@126.com>
2022-10-17 16:37:32 +0800

    Refine code structure and fix build (#2)

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,4 @@ ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/..
 rm -rf $ROOT/observer-plugins/metric-server/observer-metric-server;
 rm -rf $ROOT/observer-plugins/prometheus/observer-prometheus-server;
 rm -rf $ROOT/executor-plugins/resource-tagger/resource-tagger;
-rm -rf $ROOT/executor-plugins/resource-tagger/resource-tagger-client;
-docker rmi kubearbiter/observer-metric-server:$1 kubearbiter/observer-prometheus-server:$1 kubearbiter/kubearbiter/executor-resource-tagger:$1 2> /dev/null || true;
-
+docker rmi kubearbiter/observer-metric-server:$1 kubearbiter/observer-prometheus-server:$1 kubearbiter/executor-resource-tagger:$1 2> /dev/null || true;
